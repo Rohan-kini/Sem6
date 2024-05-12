822ea7f580d6a2147e6384626d38464888fd5d68
R K <coursesroshini@gmail.com>
2024-05-12 18:16:38 +0530

    revision

--- AA/aggregate.py
+++ AA/aggregate.py
@@ -1,37 +1,39 @@
 arr=[8,3,1,9,8,7,3,6]
 stack=[]
-stack_len=8
+stack_len=6
 unit=[]
 operations_count=0
 
 def push(i):
-    global operations_count, unit
+    global operations_count,unit
     stack.append(i)
-    unit.append(1)
     operations_count+=1
-    print(f"Stack is {stack} after pushing {i}")
+    unit.append(1)
+    print(f"Stack after pushing {i} is {stack}")
+
 
 def pop():
     global operations_count
     if stack:
         stack.pop()
-        operations_count+=1
+    operations_count+=1
 
 def multipop(k):
-    global operations_count, unit
+    global operations_count,unit
     no_of_elements=min(k,len(stack))
-    for _ in range (no_of_elements):
+    for _ in range(no_of_elements):
         pop()
-    print(f"The stack is {stack} after popping {no_of_elements} element")
     unit.append(no_of_elements)
+    print(f"Stack after multipop is {stack}")
 
+    
 
 for i in arr:
-    print(f"Element {i}:")
+    print(f"Element:{i}")
     if i<=len(stack):
-        print(f"As i is less than {len(stack)} we mulipop {i} elements from stack")
+        print(f"As {i} is less than {len(stack)} we perform multipop {i} times")
         multipop(i)
     push(i)
 
-
-print(f"T(n)={sum(unit)} and number of operations is {operations_count}")
+print(f"T(n)={sum(unit)} and num of operations is {operations_count}")
+print(f"Time complexity O({sum(unit)//operations_count})")
