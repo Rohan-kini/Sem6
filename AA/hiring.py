b815ac04ef078d155837e581c4b1bddad797437a
R K <coursesroshini@gmail.com>
2024-05-12 18:30:38 +0530

    revision

--- AA/hiring.py
+++ AA/hiring.py
@@ -2,41 +2,41 @@ import random
 
 print("Normal Approach")
 candidates=[0,1,2,3,4,5,6,7,8,9]
-print(f"candidates order:{candidates}")
+print(candidates)
 interviewed_candidates=[]
-hired_candidates=[]
+hird_candidates=[]
 
 for candidate in candidates:
     interviewed_candidates.append(candidate)
-    if not hired_candidates or candidate>max(hired_candidates):
-        hired_candidates.append(candidate)
+    if not hird_candidates or candidate>max(hird_candidates):
+        hird_candidates.append(candidate)
 
-firing_cost=len(hired_candidates)-1
+firing_cost=len(hird_candidates)-1
 print(interviewed_candidates)
-print(hired_candidates)
-print(f"Firing cost:{firing_cost}")
+print(hird_candidates)
+print(f"Firing cost={firing_cost}")
 
 
-##part2
 print("Randomized Approach")
 candidates=[0,1,2,3,4,5,6,7,8,9]
-print(f"candidates order:{candidates}")
+print(candidates)
 interviewed_candidates=[]
-hired_candidates=[]
+hird_candidates=[]
 
-for i in range(len(candidates)):
+for i in range (len(candidates)):
     selected_candidate=random.choice(candidates)
     interviewed_candidates.append(selected_candidate)
     candidates.remove(selected_candidate)
 
 max_candidate=-1
+
 for candidate in interviewed_candidates:
     if candidate>max_candidate:
         max_candidate=candidate
-        hired_candidates.append(candidate)
+        hird_candidates.append(candidate)
 
-firing_cost=len(hired_candidates)-1
+firing_cost=len(hird_candidates)-1
 print(interviewed_candidates)
-print(hired_candidates)
-print(f"Firing cost:{firing_cost}")
+print(hird_candidates)
+print(f"Firing cost={firing_cost}")
 
