684e1c9ed820263c2f72e108d4a1d76a5b11c112
R K <coursesroshini@gmail.com>
2024-05-12 20:20:20 +0530

    revised KD un jhaad

--- AA/kd_unbalanced.py
+++ AA/kd_unbalanced.py
@@ -1,29 +1,30 @@
-def insert(root, point, axis=0):
-    
+
+def insert(root,point,axis=0):
+
     if root is None:
-        return {"point": point, "axis": axis, "left": None, "right": None}
+        return {"point":point,"axis":axis,"left":None,"right":None}
     
-    if point[axis] < root["point"][axis]:
-        root["left"] = insert(root["left"], point, (axis + 1) % len(point))
+    if point[axis]<root["point"][axis]:
+        root["left"]=insert(root["left"],point,(axis+1)%len(point))
     else:
-        root["right"] = insert(root["right"], point, (axis + 1) % len(point))
+        root["right"]=insert(root["right"],point,(axis+1)%len(point))
     
     return root
 
-def print_tree(node, level=0, side=None):
+def print_tree(node,level=0,side=None):
     
     if node is not None:
-        prefix = ""
+        prefix=""
         if side is not None:
-            prefix = side + "---- "
+            prefix=side+"---"
         
-        print("   " * level + prefix + str(node["point"]))
-        print_tree(node["left"], level + 1, "L")
-        print_tree(node["right"], level + 1, "R")
+        print("  "*level+ prefix+ str(node["point"]))
+        print_tree(node["left"],level+1,"L")
+        print_tree(node["right"],level+1,"R")
 
-# Example usage
-points = [[3, 6], [17, 15], [13, 15], [6, 12], [9, 1], [2, 7], [10, 19]]
-root = None
+
+points = [(17,12),(1,1),(16,8),(5,4),(18,11),(19,2)]
+root=None
 for point in points:
-    root = insert(root, point, 0)
+    root=insert(root,point,0)
 print_tree(root)
