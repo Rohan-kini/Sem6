c0076b56ff34c266b233da8df1179944749faaf1
R K <coursesroshini@gmail.com>
2024-05-17 13:40:55 +0530

    modified for e

--- IS/rsa.py
+++ IS/rsa.py
@@ -1,13 +1,23 @@
-def rsa(p,q,e,pt):
+import math
+
+def rsa(p,q,pt):
     n=p*q
     phi=(p-1)*(q-1)
+    
+    e=2
+    while e<phi:
+        if math.gcd(e,phi)==1:
+            break
+        e+=1
+    print(e)
+
     d=1
     while True:
         if (d*e)%phi==1:
             break
         d+=1
-    
     print(d)
+
     print(f"Public key:{(e,n)}")
     print(f"Private key:{(d,n)}")
 
@@ -20,6 +30,5 @@ def rsa(p,q,e,pt):
 if __name__=="__main__":
     p=int(input("Enter value of p:"))
     q=int(input("Enter value of q:"))
-    e=int(input("Enter value of e:"))
     pt=int(input("Enter value of pt:"))
-    rsa(p,q,e,pt)
+    rsa(p,q,pt)
